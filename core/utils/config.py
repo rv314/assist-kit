d2d30ed91ba51bdd79dc62a507686bafce461a73
rv314 <rvp314@gmail.com>
2025-07-09 11:52:09 -0400

    updated config file dir

--- core/utils/config.py
+++ core/utils/config.py
@@ -3,6 +3,9 @@ import os
 
 def load_config():
   base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-  config_path = os.path.join(base_dir, "../config.json")
+  config_path = os.path.join(base_dir, "config/config.json")
   with open(config_path, "r") as f:
     return json.load(f)
+  
+# if __name__ == "__main__":
+#   load_config()
