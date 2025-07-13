e33dbd727cee1a06d226109642d1d40587a0eb54
rv314 <rvp314@gmail.com>
2025-07-12 23:18:37 -0400

    moved rule checks out in utils

--- core/guardrails/injection_detector.py
+++ core/guardrails/injection_detector.py
@@ -1,12 +1,12 @@
 import yaml
 import re
+from core.utils.config import load_injection_rules
 
 class InjectionDetector:
-  def __init__(self, config=None, rules_path='config/injection_rules.yaml'):
+  def __init__(self, config=None, rules=None):
     self.enabled = config.get("guardrails", {}).get("enable_injection_detector", True)
-    with open(rules_path, "r") as f:
-      rules = yaml.safe_load(f)
-    self.suspicious_patterns = rules.get("injection_detector", {}).get("suspicious_patterns", [])
+    self.rules = load_injection_rules()
+    self.suspicious_patterns = self.rules.get("injection_detector", {}).get("suspicious_patterns", [])
 
 
   def is_safe(self, user_input: str) -> bool:
