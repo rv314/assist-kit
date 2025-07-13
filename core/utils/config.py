82f43b57fdb7576189c43b7559292454d90a1085
rv314 <rvp314@gmail.com>
2025-07-12 23:19:08 -0400

    added function to load rules from yaml

--- core/utils/config.py
+++ core/utils/config.py
@@ -1,11 +1,18 @@
 import json
 import os
+import yaml
 
 def load_config():
   base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
   config_path = os.path.join(base_dir, "config/config.json")
   with open(config_path, "r") as f:
     return json.load(f)
+  
+def load_injection_rules():
+  base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+  rules_path = os.path.join(base_dir, "config/injection_rules.yaml")
+  with open(rules_path, "r") as f:
+      return yaml.safe_load(f)
   
 if __name__ == "__main__":
   load_config()
