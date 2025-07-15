70eec0fa12dad558ed126055e1e948c20dd6809a
rv314 <rvp314@gmail.com>
2025-07-14 20:59:17 -0400

    session_id kept optional as it may not exist originally

--- api/chat.py
+++ api/chat.py
@@ -4,17 +4,18 @@ from core.engine.chat_client import ChatEngine
 from core.utils.config import load_config
 from core.engine.session_manager import SessionManager
 from core.guardrails.guard_manager import GuardManager
+from typing import Optional
 from uuid import uuid4
 
 router = APIRouter()
 config = load_config()
 chat_engine = ChatEngine(config=config)
-session_manager = SessionManager(config)
+session_manager = SessionManager(config=config.get("session_store", {}))
 guard_manager = GuardManager(config)
 
 # --------- Request and Response Schema ----------
 class ChatRequest(BaseModel):
-  session_id: str = str(uuid4())
+  session_id: Optional[str] = None
   message: str
 
 class ChatResponse(BaseModel):
@@ -26,7 +27,8 @@ class ChatResponse(BaseModel):
 # ------- Endpoint --------
 @router.post("/chat", response_model=ChatResponse)
 def chat(request: ChatRequest):
-  session_id = request.session_id
+  # print(f">>> Received message: {request.message}")
+  session_id = request.session_id or str(uuid4())
   message = request.message
 
   # GuardRails
@@ -43,13 +45,16 @@ def chat(request: ChatRequest):
   
   # Load session
   messages = session_manager.load_session(session_id)
-  messages.append({"role": "user", "content": request.message})
+  if not messages:
+    messages = []
+  messages.append({"role": "user", "content": message})
 
   # Call chat engine
   reply = chat_engine.chat(messages)
+  # print(f" Assistant response: {reply}")
   messages.append({"role": "assistant", "content" : reply})
 
   # Save session
-  session_manager.save_session(request.session_id, messages)
+  session_manager.save_session(session_id, messages)
 
   return ChatResponse(response=reply)
