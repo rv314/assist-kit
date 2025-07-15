a16ab17c8bfe3dd3639af20532fc7e6ebf28f951
rv314 <rvp314@gmail.com>
2025-07-15 18:47:19 -0400

    changed variable names for clarity

--- core/engine/session_manager.py
+++ core/engine/session_manager.py
@@ -5,9 +5,9 @@ from core.session_backends.json_store import JSONSessionStore
 class SessionManager:
   def __init__(self, full_config):
     session_store_config = full_config.get("session_store", {})
-    backend_name = session_store_config.get("provider", "json")
-    backend_config = session_store_config.get("config", {})
-    self.store = get_session_backend(backend_name, config=backend_config)
+    session_store_backend_name = session_store_config.get("provider", "json")
+    session_store_backend_config = session_store_config.get("config", {})
+    self.store = get_session_backend(session_store_backend_name, config=session_store_backend_config)
 
 
   def load_session(self, session_id: str) -> list:
