1ea9315f707a2ac101ecb61f2cb618379491e72b
rv314 <rvp314@gmail.com>
2025-07-15 16:09:33 -0400

    using pydantic for data validation, bleach.clean for HTML sanity checks

--- api/chat.py
+++ api/chat.py
@@ -1,35 +1,56 @@
-from fastapi import APIRouter, Request, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI, APIRouter, Request, HTTPException
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, constr, Field, ValidationError
+
+from typing import Optional, Annotated
+from uuid import UUID, uuid4
+from bleach import clean
+
 from core.engine.chat_client import ChatEngine
 from core.utils.config import load_config
 from core.engine.session_manager import SessionManager
 from core.guardrails.guard_manager import GuardManager
-from typing import Optional
-from uuid import uuid4
+
 
 router = APIRouter()
+
+app = FastAPI()
+app.include_router(router)
+
 config = load_config()
 chat_engine = ChatEngine(config=config)
-session_manager = SessionManager(config=config.get("session_store", {}))
+session_manager = SessionManager(config)
 guard_manager = GuardManager(config)
 
 # --------- Request and Response Schema ----------
 class ChatRequest(BaseModel):
-  session_id: Optional[str] = None
-  message: str
+  session_id: UUID = Field(default_factory=uuid4)
+  message: Annotated[str, constr(strip_whitespace=True, min_length=1, max_length=1000)]
 
 class ChatResponse(BaseModel):
-  response: str
+  response: Annotated[str, constr(strip_whitespace=True)]
   allowed: bool = True
-  reason: str = ""
-  details: str = ""
+  reason: Optional[str] = ""
+  details: Optional[str] = ""
+
+# --------- Error Handler ----------------
+@app.exception_handler(ValidationError)
+async def validation_exception_handler(request: Request, exc: ValidationError):
+  return JSONResponse(
+    status_code=422,
+    content={
+      "error": "Validation Failed",
+      "details": exc.errors(),
+    },
+  )
 
 # ------- Endpoint --------
 @router.post("/chat", response_model=ChatResponse)
 def chat(request: ChatRequest):
   # print(f">>> Received message: {request.message}")
   session_id = request.session_id or str(uuid4())
-  message = request.message
+  ## HTML sanitization using "clean" to strip dangerous tags
+  message = clean(request.message)
 
   # GuardRails
   guard_result = guard_manager.get_all_guard_results(message, session_id)
