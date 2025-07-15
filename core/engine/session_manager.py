2ec47dd4c09126e0a9347661f768d2e95f88ca98
rv314 <rvp314@gmail.com>
2025-07-15 16:09:54 -0400

    pre-defined config : session store

--- core/engine/session_manager.py
+++ core/engine/session_manager.py
@@ -3,9 +3,10 @@ from core.session_backends.json_store import JSONSessionStore
 
 
 class SessionManager:
-  def __init__(self, config):
-    backend_name = config.get("session_store", {}).get("provider", "json")
-    backend_config = config.get("session_store", {}).get("config", {})
+  def __init__(self, full_config):
+    session_store_config = full_config.get("session_store", {})
+    backend_name = session_store_config.get("provider", "json")
+    backend_config = session_store_config.get("config", {})
     self.store = get_session_backend(backend_name, config=backend_config)
 
 
