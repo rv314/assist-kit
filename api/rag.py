7083b58b86fc861c2f7df347227e7b6f593c7e44
rv314 <rvp314@gmail.com>
2025-07-24 19:40:41 -0400

    generic tempfile logic added

--- api/rag.py
+++ api/rag.py
@@ -1,22 +1,24 @@
 from fastapi import FastAPI, APIRouter, UploadFile, File, Form, HTTPException
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+import tempfile
 
-from typing import Optional
+from typing import Optional, Dict, List
 from uuid import UUID, uuid4
 from bleach import clean
 
 from core.engine.rag_engine import RAGEngine
+from core.engine.vector_store import VectorStore
 from core.utils.config import load_config
+from core.registries.embedding_registry import get_embedder
 
 router = APIRouter()
-app = FastAPI()
-app.include_router(router)
 
 # Load config
 config = load_config()
 rag_engine = RAGEngine(config=config)
-
+embedder = get_embedder(config["embedding"]["provider"])
+vector_store = VectorStore(full_config=config, embedder=embedder)
 # ------------ Request / Response Models -----------------
 class RagQueryRequest(BaseModel):
   query: str
@@ -31,19 +33,38 @@ class RagIngestResponse(BaseModel):
   message: str
   chuncks_stored: int
 
+class TestChunk(BaseModel):
+  id: str
+  text: str
+  metadata: Dict[str, str]
+
 # ------------- Endpoints -------------------------------
+## Testing
+@router.post("/rag/test-ingest")
+async def test_ingest(chunks: List[TestChunk]):
+  print(chunks)
+  try:
+      vector_store.add_documents([
+          {"id": chunk.id, "text": chunk.text, "metadata": chunk.metadata}
+          for chunk in chunks
+      ])
+  except Exception as e:
+      raise HTTPException(status_code=500, detail=f"Test ingestion failed: {str(e)}")
+
+  return {"message": "Test ingestion succeeded", "chunks": len(chunks)}
+
 @router.post("/rag/ingest", response_model=RagIngestResponse)
 async def ingest(file: UploadFile = File(...)):
   if not file.filename.endswith(".pdf"):
     raise HTTPException(status_code=400, detail="Only PDF files are supported")
   
-  # Save uploaded file temporarily
-  temp_path = f"/tmp/{file.filename}"
-  with open(temp_path, "wb") as f:
-    content = await file.read()
-    f.write(content)
-
   try:
+    # Save uploaded file temporarily
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp:
+      temp.write(await file.read())
+      temp_path = temp.name
+
+  
     chunks = rag_engine.ingest_pdf(temp_path)
   except Exception as e:
     raise HTTPException(status_code=500, detail=f"Ingestion Failed: {str(e)}")
@@ -54,7 +75,8 @@ async def ingest(file: UploadFile = File(...)):
 @router.post("/rag/query", response_model=RagQueryResponse)
 async def query_rag(request: RagQueryRequest):
   try:
-    answer = rag_engine.query(request.query, top_k=request.top_k)
+    query = clean(request.query, tags=[], attributes={}, strip=True)
+    answer = rag_engine.query(query, top_k=request.top_k)
   except Exception as e:
     raise HTTPException(status_code=500, detail=f"Query Failed: {str(e)}")
   
