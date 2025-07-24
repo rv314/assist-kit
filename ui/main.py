44a388bfcc7825902ea596989b9e42c79b108991
rv314 <rvp314@gmail.com>
2025-07-24 19:41:51 -0400

    standardized dark-mode. needs to be included in pages

--- ui/main.py
+++ ui/main.py
@@ -4,18 +4,29 @@ from ui.pages.chat import Chat
 
 dark_mode_state = None
 
+def setup_dark_mode():
+    global dark_mode_state
+    dark = ui.dark_mode().bind_value(app.storage.user, 'dark_mode')
+    ui.switch('Dark Mode').bind_value(dark)
+
+    @dark.on_value_change
+    def on_change():
+        nonlocal dark
+        dark_mode_state = dark.value
+        
 # Route definitions
 @ui.page("/")
 def route_home():
     home.render()
+    setup_dark_mode()
 
-    def update_dark_mode_state():
+"""     def update_dark_mode_state():
         global dark_mode_state
         dark_mode_state = dark.value
     dark = ui.dark_mode().bind_value(app.storage.user, 'dark_mode')
     ui.switch('Dark mode').bind_value(dark)
 
-    dark.on_value_change(update_dark_mode_state)
+    dark.on_value_change(update_dark_mode_state) """
 
 @ui.page("/chat")
 def route_chat():
